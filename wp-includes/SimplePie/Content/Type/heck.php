62e4729741a4f9d205518fa75ab35a30e8e2bed6
Raju Dhoni <32018260+rajusiripalli@users.noreply.github.com>
2022-06-18 21:32:09 +0530

    rajesh safety nets

---
+++ wp-includes/SimplePie/Content/Type/heck.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    
+<title>Error</title>
+
+    
+    <meta name="description" content="Markdown pastebin service with preview, custom urls and editing. Fast, simple and free.">
+    <meta name="keywords" content="pastebin, markdown, publishing, markdown pastebin, markdown from command line">
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:description" content="Markdown pastebin service with preview, custom urls and editing." />
+    <meta name="twitter:title" content="Rentry.co - Markdown Pastebin" />
+    <meta name="twitter:site" content="@rentry_co" />
+    <meta name="twitter:image" content="https://rentry.co/static/icons/512.png" />
+
+    <meta property="og:url" content="https://rentry.co/" />
+    <meta property="og:title" content="Rentry.co - Markdown Pastebin" />
+    <meta property="og:description" content="Markdown pastebin service with preview, custom urls and editing." />
+    <meta property="og:image" content="https://rentry.co/static/icons/270.png" />
+    <meta property="og:type" content="website" />
+    
+    <meta name="referrer" content="strict-origin-when-cross-origin" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=1" />
+
+    <link rel="stylesheet" href="/static/css/bootstrap.min.css?v=68">
+    <link rel="manifest" href="/static/manifest.json?v=8">
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-102083007-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-102083007-1');
+
+    </script>
+</head>
+
+<body class="m-0 p-0">
+
+    <div class="container container-smooth">
+        <div class="row no-gutters">
+            
+<div class="col-12">
+    <div class="row no-gutters">
+        <div class="p-2 my-5 mx-auto text-center">
+            <h1 class="h4 text-gray-dark my-3 no-pointer">Error</h1>
+            <span class="h5 text-gray-dark font-weight-normal m-0">404 not found</span>
+        </div>
+    </div>
+</div>
+
+
+
+            
+            <div class="text-center w-100 mb-3">
+                <hr class="basement-hr">
+                <a class="mr-1" href="/">new</a>&middot;<a class="mx-1" href="/what">what</a>&middot;<a class="mx-1" href="/how">how</a>&middot;<a class="ml-1" href="/langs">langs</a>
+
+            </div>
+            
+            
+        </div>
+    </div>
+
+    <script src="/static/js/jquery.min.js?v=13"></script>
+    <script src="/static/js/bootstrap.min.js?v=13"></script>
+    
+</body>
+
+</html>
